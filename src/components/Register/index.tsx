db03efe7ff5b74bcc26c9fbc1c761c059de28de0
Crong (SungEun Kim) <70146617+sinde530@users.noreply.github.com>
2022-10-16 23:44:11 +0900

    주석삭제

--- src/components/Register/index.tsx
+++ src/components/Register/index.tsx
@@ -18,8 +18,8 @@ export default function Register() {
   const [mismatchError, setMismatchError] = useState<boolean>(false);
 
   const handleChangeEmail = useCallback((e: any) => {
-    // const emailRegex =
-    //   /([\w-.]+)@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.)|(([\w-]+\.)+))([a-zA-Z]{2,4}|[0-9]{1,3})(\]?)$/;
+    const emailRegex =
+      /([\w-.]+)@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.)|(([\w-]+\.)+))([a-zA-Z]{2,4}|[0-9]{1,3})(\]?)$/;
 
     setEmail(e.target.value);
   }, []);
